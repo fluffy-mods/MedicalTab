21557d151b5212212f0aaba9bb22c82e48922269
Fluffy <fluffy.l2032@gmail.com>
2019-07-24 15:31:29 +0200

    fix posibble nullref in MedicalCare.get_OverallCare
    should resolve #35

--- Source/MedicalInfo/ColumnWorkers/PawnColumnWorker_MedicalCare.cs
+++ Source/MedicalInfo/ColumnWorkers/PawnColumnWorker_MedicalCare.cs
@@ -11,98 +11,22 @@ namespace Fluffy
 {
     public class PawnColumnWorker_MedicalCare : PawnColumnWorker, OptionalColumn
     {
-        // todo; try to get rid of the EventType == Layout bail for DoColumn() to see if that improves responsiveness
-
-        #region Properties
-
         public MedicalCareCategory OverallCare
         {
             get
             {
-                return MainTabWindow_Medical.Instance.Table.PawnsListForReading.Max(p => p.playerSettings.medCare);
+                return MainTabWindow_Medical.Instance?.Table?.PawnsListForReading?.Max( p => p.playerSettings.medCare ) ?? MedicalCareCategory.Best;
             }
             set
             {
-                MainTabWindow_Medical.Instance.Table.PawnsListForReading.ForEach(p => p.playerSettings.medCare = value);
+                MainTabWindow_Medical.Instance.Table.PawnsListForReading.ForEach(
+                    p => p.playerSettings.medCare = value );
             }
         }
 
-        #endregion Properties
-
-        #region Methods
-
-        public override int Compare(Pawn a, Pawn b)
+        public bool ShowFor( SourceType source )
         {
-            return GetValueToCompare(a).CompareTo(GetValueToCompare(b));
-        }
-
-        public override void DoCell(Rect rect, Pawn pawn, PawnTable table)
-        {
-            if ( pawn?.playerSettings?.medCare != null )
-                MedicalCareUtility.MedicalCareSetter(rect, ref pawn.playerSettings.medCare );
-        }
-
-        public void DoDefaultMedCareHeader(Rect rect)
-        {
-            switch (MainTabWindow_Medical.Instance.Source)
-            {
-                case SourceType.Animals:
-                    MedicalCareUtility.MedicalCareSetter(rect, ref Find.PlaySettings.defaultCareForColonyAnimal);
-                    break;
-
-                case SourceType.Colonists:
-                    MedicalCareUtility.MedicalCareSetter(rect, ref Find.PlaySettings.defaultCareForColonyHumanlike);
-                    break;
-
-                case SourceType.Hostiles:
-                    MedicalCareUtility.MedicalCareSetter(rect, ref Find.PlaySettings.defaultCareForHostileFaction);
-                    break;
-
-                case SourceType.Prisoners:
-                    MedicalCareUtility.MedicalCareSetter(rect, ref Find.PlaySettings.defaultCareForColonyPrisoner);
-                    break;
-
-                case SourceType.Visitors:
-                    MedicalCareUtility.MedicalCareSetter(rect, ref Find.PlaySettings.defaultCareForNeutralFaction);
-                    break;
-            }
-        }
-
-        public override void DoHeader(Rect rect, PawnTable table)
-        {
-            // decrease height of rect (base does this already, but MedCareSetter does not.
-            rect.yMin = rect.yMax - Constants.DesiredHeaderHeight;
-
-            if (Event.current.shift && Mouse.IsOver(rect) && table.PawnsListForReading.Any())
-            {
-                // mass assign
-                // note; weird as fuck sentinel/token approach, because somehow an intercepted click event and the medCareSetter do not fire in the same GUI phase?
-                MedicalCareCategory sentinel = OverallCare;
-                MedicalCareCategory token = table.PawnsListForReading.Max(p => p.playerSettings.medCare);
-                MedicalCareUtility.MedicalCareSetter(rect, ref token);
-                if (sentinel != token)
-                    OverallCare = token;
-
-                TooltipHandler.TipRegion(rect, GetHeaderTip(table));
-            }
-            else if (Event.current.control && Mouse.IsOver(rect))
-            {
-                // defaults
-                DoDefaultMedCareHeader(rect);
-                TooltipHandler.TipRegion(rect, GetHeaderTip(table));
-            }
-            else
-            {
-                // text
-                base.DoHeader(rect, table);
-            }
-        }
-
-        public override int GetMinWidth(PawnTable table) => Constants.MedicalCareSetterWidth;
-
-        public bool ShowFor(SourceType source)
-        {
-            switch (source)
+            switch ( source )
             {
                 case SourceType.Hostiles:
                     return false;
@@ -112,28 +36,101 @@ namespace Fluffy
             }
         }
 
-        internal int GetValueToCompare(Pawn pawn)
+        public override int Compare( Pawn a, Pawn b )
         {
-            return (int)pawn.playerSettings.medCare;
+            return GetValueToCompare( a ).CompareTo( GetValueToCompare( b ) );
         }
 
-        protected override string GetHeaderTip(PawnTable table)
+        public override void DoCell( Rect rect, Pawn pawn, PawnTable table )
         {
-            string tip = base.GetHeaderTip(table);
+            if ( pawn?.playerSettings?.medCare != null )
+                MedicalCareUtility.MedicalCareSetter( rect, ref pawn.playerSettings.medCare );
+        }
+
+        public void DoDefaultMedCareHeader( Rect rect )
+        {
+            switch ( MainTabWindow_Medical.Instance.Source )
+            {
+                case SourceType.Animals:
+                    MedicalCareUtility.MedicalCareSetter( rect, ref Find.PlaySettings.defaultCareForColonyAnimal );
+                    break;
+
+                case SourceType.Colonists:
+                    MedicalCareUtility.MedicalCareSetter( rect, ref Find.PlaySettings.defaultCareForColonyHumanlike );
+                    break;
+
+                case SourceType.Hostiles:
+                    MedicalCareUtility.MedicalCareSetter( rect, ref Find.PlaySettings.defaultCareForHostileFaction );
+                    break;
+
+                case SourceType.Prisoners:
+                    MedicalCareUtility.MedicalCareSetter( rect, ref Find.PlaySettings.defaultCareForColonyPrisoner );
+                    break;
+
+                case SourceType.Visitors:
+                    MedicalCareUtility.MedicalCareSetter( rect, ref Find.PlaySettings.defaultCareForNeutralFaction );
+                    break;
+            }
+        }
+
+        public override void DoHeader( Rect rect, PawnTable table )
+        {
+            // decrease height of rect (base does this already, but MedCareSetter does not.
+            rect.yMin = rect.yMax - Constants.DesiredHeaderHeight;
+
+            if ( Event.current.shift && Mouse.IsOver( rect ) && table.PawnsListForReading.Any() )
+            {
+                // mass assign
+                // note; weird as fuck sentinel/token approach, because somehow an intercepted click event and the medCareSetter do not fire in the same GUI phase?
+                var sentinel = OverallCare;
+                var token    = table.PawnsListForReading.Max( p => p.playerSettings.medCare );
+                MedicalCareUtility.MedicalCareSetter( rect, ref token );
+                if ( sentinel != token )
+                    OverallCare = token;
+
+                TooltipHandler.TipRegion( rect, GetHeaderTip( table ) );
+            }
+            else if ( Event.current.control && Mouse.IsOver( rect ) )
+            {
+                // defaults
+                DoDefaultMedCareHeader( rect );
+                TooltipHandler.TipRegion( rect, GetHeaderTip( table ) );
+            }
+            else
+            {
+                // text
+                base.DoHeader( rect, table );
+            }
+        }
+
+        public override int GetMinWidth( PawnTable table )
+        {
+            return Constants.MedicalCareSetterWidth;
+        }
+
+        internal int GetValueToCompare( Pawn pawn )
+        {
+            return (int) pawn.playerSettings.medCare;
+        }
+
+        protected override string GetHeaderTip( PawnTable table )
+        {
+            var tip = base.GetHeaderTip( table );
             tip += "\n\n";
 
-            if (table.PawnsListForReading.Any())
+            if ( table.PawnsListForReading.Any() )
             {
-                tip += "MedicalTab.XClickToY".Translate("MedicalTab.Shift".Translate(),
-                                                         "MedicalTab.MassAssignMedicalCare".Translate())
+                tip += "MedicalTab.XClickToY".Translate( "MedicalTab.Shift".Translate(),
+                                                         "MedicalTab.MassAssignMedicalCare".Translate() )
                                              .CapitalizeFirst();
                 tip += "\n";
             }
-            tip += "MedicalTab.XClickToY".Translate("MedicalTab.Ctrl".Translate(),
-                                                     "MedicalTab.SetDefaultMedicalCare".Translate()).CapitalizeFirst();
+
+            tip += "MedicalTab.XClickToY".Translate( "MedicalTab.Ctrl".Translate(),
+                                                     "MedicalTab.SetDefaultMedicalCare".Translate() ).CapitalizeFirst();
             return tip;
         }
 
-        #endregion Methods
+        // todo; try to get rid of the EventType == Layout bail for DoColumn() to see if that improves responsiveness
     }
 }
